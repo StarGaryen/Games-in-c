371bf85e82414cb2023b06aa75b0fcc429aaf814
Devendra Samatia <devendrasamatia@gmail.com>
2017-01-10 19:27:30 +0530

    Add files via upload

---
+++ lol_game.c
@@ -0,0 +1,264 @@
+/*author JON SNOW*/
+#include<time.h>
+#include<stdio.h>
+#include<windows.h>
+#define DOWN 80
+#define UR 02
+#define UL 00
+#define DR 22
+#define DL 20
+void update_ball_cord();
+void gotoxy(short X,short Y);//set cursor position to (X,Y)
+void draw_wall(); //draw outer wall
+void reset();
+void play();
+void draw_ball(BOOL a); //draw ball if a=TRUE else removes ball
+void draw_plate(short i);// digit on ones place to decide draw or remove and digit on tens place to decide plate number
+void wall_hit();// check for ball hit on upper and lower walls
+void back_hit(); // check for ball hit on side walls
+void plate_hit();//cheak for ball hit on both plateforms
+void delay(int miliseconds);
+void lives();
+void exitgame();
+COORD plate[2]; //plate[0]==> user1 platform and plate[1]==> user2 platform
+
+struct BALL{
+        COORD coordinates;
+        int dir;
+    }ball;
+struct Score{
+	short saves;
+	short lives;
+	}score[2]={{0,3},{0,3}};
+int main()
+{
+	draw_wall();
+	lives();
+	ball.dir=UR;
+	reset();
+	draw_plate(01);
+	draw_plate(11);
+	play();
+	exitgame();
+	return 0;
+}
+void update_ball_cord()
+{
+    if(ball.dir==UR)
+    {
+        ball.coordinates.X++;
+        ball.coordinates.Y--;
+    }
+    else if(ball.dir==UL)
+    {
+        ball.coordinates.X--;
+        ball.coordinates.Y--;
+    }
+    else if(ball.dir==DR)
+    {
+        ball.coordinates.X++;
+        ball.coordinates.Y++;
+    }
+    else if(ball.dir==DL)
+    {
+        ball.coordinates.X--;
+        ball.coordinates.Y++;
+    }
+    else
+        printf("/a");
+}
+void gotoxy(short x,short y)
+{
+    COORD a;
+    a.X=x;
+    a.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),a);
+}
+void draw_wall()
+{
+    int i;
+	for(i=6;i<=71;i++)
+    {
+        gotoxy(i,1);
+        printf("%c",220);
+        gotoxy(i,23);
+        printf("%c",223);
+    }
+    for(i=2;i<23;i++)
+    {
+        gotoxy(6,i);
+        printf("%c",221);
+        gotoxy(71,i);
+        printf("%c",222);
+    }
+    gotoxy(6,24);
+}
+void reset()
+{
+	srand((unsigned) ball.coordinates.Y);
+	ball.coordinates.X=38;
+	ball.coordinates.Y=rand()%15 + 5;
+	plate[0].X=8;
+	plate[1].X=69;
+	plate[0].Y=plate[1].Y=ball.coordinates.Y;
+}
+void play()
+{
+    do{
+        wall_hit();
+        back_hit();
+        plate_hit();
+        draw_ball(TRUE);
+        delay(80);
+        draw_ball(FALSE);
+        update_ball_cord();
+    }while(TRUE);
+
+}
+void draw_ball(BOOL a)
+{
+    gotoxy(ball.coordinates.X,ball.coordinates.Y);
+    if(a)
+    printf("%c",15);
+    else
+    	printf(" ");
+}
+void draw_plate(short i)
+{
+	char k;
+	short j;
+	if(i%10)
+		k='|';
+	else
+		k=' ';
+	for(j=0;j<4;j++)
+	{
+		gotoxy(plate[i/10].X,plate[i/10].Y+ j);
+		printf("%c",k);
+	}
+}
+void wall_hit()
+{
+	if(ball.coordinates.Y==2)
+		ball.dir+=20;
+	else if(ball.coordinates.Y==22)
+		ball.dir-=20;
+}
+void back_hit()
+{
+	if(ball.coordinates.X==8)
+	{
+		score[0].lives--;
+		lives();
+		exitgame();
+	}
+	else if (ball.coordinates.X==69)
+	{
+		score[1].lives--;
+		lives();
+		exitgame();
+	}
+}
+void plate_hit()
+{
+	if(ball.coordinates.X==plate[0].X+1 && (ball.coordinates.Y==plate[0].Y+3 || ball.coordinates.Y==plate[0].Y || ball.coordinates.Y==plate[0].Y+1 || ball.coordinates.Y==plate[0].Y+2 ||((ball.coordinates.Y==plate[0].Y-1)&& ball.dir == 20) || ((ball.coordinates.Y==plate[0].Y+4)&& ball.dir == 0)))
+	{
+		ball.dir+=2;
+		score[0].saves++;
+	}
+	else if(ball.coordinates.X==plate[1].X-1 && (ball.coordinates.Y==plate[1].Y+3 || ball.coordinates.Y==plate[1].Y || ball.coordinates.Y==plate[1].Y+1 || ball.coordinates.Y==plate[1].Y+2 ||((ball.coordinates.Y==plate[1].Y-1)&& ball.dir == 22) || ((ball.coordinates.Y==plate[1].Y+4)&& ball.dir == 2)))
+	{
+		ball.dir-=2;
+		score[1].saves++;
+	}
+}
+void delay(int miliseconds)
+{
+    clock_t now,then;
+    int pause;
+    char key;
+    pause = miliseconds*(CLOCKS_PER_SEC/1000);
+    now=then=clock();
+    while(now-then < pause)
+    {
+        now=clock();
+        if(kbhit())
+        {
+            key=getch();
+            if(key=='o' && plate[1].Y>2)
+            {
+                gotoxy(plate[1].X,plate[1].Y+3);
+                printf(" ");
+                plate[1].Y--;
+                gotoxy(plate[1].X,plate[1].Y);
+                printf("|");
+            }
+            else if(key=='l' && plate[1].Y<19)
+            {
+                gotoxy(plate[1].X,plate[1].Y);
+                printf(" ");
+                plate[1].Y++;
+                gotoxy(plate[1].X,plate[1].Y+3);
+                printf("|");
+            }
+            else if(key=='w' && plate[0].Y>2)
+            {
+                gotoxy(plate[0].X,plate[0].Y+3);
+                printf(" ");
+                plate[0].Y--;
+                gotoxy(plate[0].X,plate[0].Y);
+                printf("|");
+            }
+            else if(key=='s' && plate[0].Y<19)
+            {
+                gotoxy(plate[0].X,plate[0].Y);
+                printf(" ");
+                plate[0].Y++;
+                gotoxy(plate[0].X,plate[0].Y+3);
+                printf("|");
+            }
+            else
+            {
+                printf("\a");
+            }
+        }
+    }
+}
+void lives()
+{
+    gotoxy(1,10);
+    printf("Lives\n  %d",score[0].lives);
+    gotoxy(72,10);
+    printf("Lives");
+    gotoxy(75,11);
+    printf("%d",score[1].lives);
+}
+void exitgame()
+{
+    if(score[0].lives==0)
+    {
+        system("cls");
+        printf("player 2 won\npress enter to exit");
+        getchar();
+        exit(0);
+    }
+    else if(score[1].lives==0)
+    {
+        system("cls");
+        printf("player 2 won\npress enter to exit");
+        getchar();
+        exit(0);
+    }
+    else
+    {
+        draw_plate(00);
+        draw_plate(10);
+        reset();
+        draw_plate(01);
+        draw_plate(11);
+
+        play();
+    }
+
+
+}
---
+++ snake game.c
@@ -0,0 +1,332 @@
+/*author JON SNOW*/
+#include<time.h>
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+#include<unistd.h>
+#define RIGHT 100
+#define LEFT 97
+#define UP 119
+#define DOWN 115
+int speed =1;
+char key=77;
+int lenght=2;
+int current_position=1;
+int total_bends=1;
+int life=3;
+struct position
+{
+    int x;
+    int y;
+};
+char direction;
+struct position head,body_points[50],bend_location[100],food_location;
+void instructions();
+void draw_box();
+void gotoxy(int ,int);
+void heart();
+void draw();
+void update();
+void up();
+void down();
+void right();
+void left();
+void update_rem_body();
+void wall_hit();
+void reset();
+void delay(int);
+void speedselect();
+void exitgame();
+int main()
+{
+    instructions();
+    speedselect();
+    reset();
+    system("cls");
+    draw_box();
+    draw();
+    update();
+}
+void instructions()
+{
+    gotoxy(10,7);
+    printf("you know what to do.");
+    gotoxy(10,6);
+    printf("Press any key to continue");
+    if(getchar()== 27)
+        exit(0);
+    fflush(stdin);
+}
+void draw_box()
+{
+    int i;
+	for(i=6;i<=70;i++)
+    {
+        gotoxy(i,3);
+        printf("%c",220);
+        gotoxy(i,20);
+        printf("%c",223);
+    }
+    for(i=4;i<20;i++)
+    {
+        gotoxy(6,i);
+        printf("%c",221);
+        gotoxy(70,i);
+        printf("%c",222);
+    }
+    gotoxy(3,21);
+}
+void gotoxy(int x,int y)
+{
+    COORD a;
+    a.X=x;
+    a.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),a);
+}
+void heart()
+{
+    int i=1;
+    if(head.x==food_location.x && head.y == food_location.y)
+    {
+        lenght++;
+        i=rand()%70;
+        if(i<6)
+            i+=10;
+        food_location.x=i;
+        i=rand()%20;
+        if(i<3)
+            i+=6;
+        food_location.y=i;
+    }
+}
+void draw_snake()
+{
+    int i;
+    gotoxy(head.x,head.y);
+    printf("%c",16);
+    for(i=0;i<lenght;i++)
+    {
+        if(head.x==body_points[i].x && body_points[i].y == head.y)
+        {
+            exitgame();
+        }
+        else
+        {
+            gotoxy(body_points[i].x,body_points[i].y);
+            printf("%c",16);
+        }
+    }
+}
+void remove_snake()
+{
+    int i;
+    gotoxy(head.x,head.y);
+    printf(" ");
+    for(i=0;i<lenght;i++)
+    {
+        gotoxy(body_points[i].x,body_points[i].y);
+        printf(" ");
+    }
+}
+void draw()
+{
+    int i;
+    heart();
+    gotoxy(food_location.x,food_location.y);
+    printf("%c",3);
+    gotoxy(6,1);
+    printf("YOUR scanty SCORE : %d" ,lenght-2);
+    gotoxy(6,22);
+    printf("Lives : %d",life);
+}
+void update()
+{ int i;
+    do
+    {
+            current_position=1;
+        if (direction ==UP)
+            up();
+        else if (direction==DOWN)
+            down();
+        else if(direction ==RIGHT)
+            right();
+        else if(direction== LEFT)
+            left();
+        wall_hit();
+        draw();
+        draw_snake();
+        delay(1000/(speed*7));
+        remove_snake();
+        fflush(stdin);
+    }while(!kbhit());
+    key=getch();
+    for(i=0;i<lenght;i++)
+    {
+        body_points[i].x=0;
+        body_points[i].y=0;
+    }
+    if((direction==RIGHT || direction==LEFT) && ( key==UP || key==DOWN ))
+    {
+        direction=key;
+        total_bends++;
+        bend_location[total_bends-1]=head;
+    }
+     else if((direction==UP || direction == DOWN )&& (key== LEFT || key==RIGHT ))
+    {
+        direction=key;
+        total_bends++;
+        bend_location[total_bends-1]=head;
+    }
+     else
+     {
+            gotoxy(6,22);
+            printf("\a%d",key);
+     }
+    update();
+}
+void up()
+{
+    head.y--;
+    while(current_position<=lenght && (head.y + current_position) < bend_location[total_bends-1].y)
+    {
+        body_points[current_position-1].x= head.x;
+        body_points[current_position-1].y=head.y + current_position;
+        current_position++;
+    }
+    update_rem_body();
+}
+void down()
+{
+    head.y++;
+    while(current_position<= lenght && (head.y - current_position) > bend_location[total_bends-1].y)
+    {
+        body_points[current_position-1].x= head.x;
+        body_points[current_position-1].y=head.y -current_position ;
+        current_position++;
+    }
+    update_rem_body();
+}
+void right()
+{
+    head.x++;
+    while(current_position<=lenght && (head.x-current_position)>bend_location[total_bends-1].x)
+    {
+        body_points[current_position-1].x=head.x-current_position;
+        body_points[current_position-1].y=head.y;
+        current_position++;
+    }
+    update_rem_body();
+}
+void left()
+{
+    head.x--;
+    while(current_position<=lenght && (head.x+current_position)<bend_location[total_bends-1].x)
+    {
+        body_points[current_position-1].x=head.x+current_position;
+        body_points[current_position-1].y=head.y;
+        current_position++;
+    }
+    update_rem_body();
+}
+void update_rem_body()
+{
+    int i ,q;
+    for(i=total_bends-1;i >0 && current_position <= lenght;i--)
+    {
+        if(bend_location[i].x==bend_location[i-1].x &&  bend_location[i].y>bend_location[i-1].y)
+        {
+            for(q=1; bend_location[i].y- bend_location[i-1].y >= q && current_position<=lenght;q++)
+            {
+                body_points[current_position-1].x = bend_location[i].x;
+                body_points[current_position-1].y=bend_location[i].y - q;
+                current_position++;
+            }
+        }
+        else if(bend_location[i].x==bend_location[i-1].x &&  bend_location[i].y<bend_location[i-1].y )
+        {
+            for(q=1; -(bend_location[i].y- bend_location[i-1].y )>= q && current_position<= lenght;q++)
+            {
+                body_points[current_position-1].x = bend_location[i].x;
+                body_points[current_position-1].y=bend_location[i].y + q;
+                current_position++;
+            }
+        }
+        else if(bend_location[i].y==bend_location[i-1].y &&  bend_location[i].x >bend_location[i-1].x)
+        {
+            for(q=1; bend_location[i].x- bend_location[i-1].x >= q && current_position<=lenght;q++)
+            {
+                body_points[current_position-1].x = bend_location[i].x -q;
+                body_points[current_position-1].y=bend_location[i].y ;
+                current_position++;
+            }
+        }
+        else if(bend_location[i].y==bend_location[i-1].y &&  bend_location[i].x<bend_location[i-1].x)
+        {
+            for(q=1;bend_location[i-1].x- bend_location[i].x >= q && current_position<=lenght;q++)
+            {
+                body_points[current_position-1].x = bend_location[i].x+q;
+                body_points[current_position-1].y=bend_location[i].y ;
+                current_position++;
+            }
+        }
+        else
+            {
+            printf("cant set points");
+            }
+    }
+}
+void wall_hit()
+{
+    if(head.x==6 || head.x==70 || head.y==3 || head. y == 20)
+       exitgame();
+
+}
+void reset()
+{
+    head.x=13;
+    head.y=5;
+    direction=RIGHT;
+    food_location.x=16;
+    food_location.y=5;
+    body_points[0].x=12;
+    body_points[0].y=5;
+    body_points[1].x=11;
+    body_points[1].y=5;
+    bend_location[0].x=11;
+    bend_location[0].y=5;
+    lenght =2;
+    total_bends=1;
+}
+void delay(int milliseconds)
+{
+    long pause;
+    clock_t now,then;
+
+    pause = milliseconds*(CLOCKS_PER_SEC/1000);
+    now = then = clock();
+    while( (now-then) < pause )
+        now = clock();
+}
+void exitgame()
+{
+    life--;
+    system("cls");
+    gotoxy(10,5);
+    printf("You Scored : %d \t\t Lives Remaining : %d",lenght-2,life);
+    if(life>0)
+        main();
+    else
+    {
+        gotoxy(10,6);
+        printf("press any key to exit ..");
+        getchar();
+        exit(0);
+    }
+}
+void speedselect()
+{
+    system("cls");
+    printf("Choose Difficulty\n1. EASY\n2. MEDIUM\n3. HARD\n4. I'm a  PRO%c%c%c",1,2,1);
+    scanf("%d",&speed);
+    fflush(stdin);
+}
